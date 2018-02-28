d46092e1b017a13cdc70b26db2d524bbcf43f53c
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-03-01 03:01:16 +0700

    add giao diện

---
+++ views/main/include/css.php
@@ -0,0 +1,5 @@
+<!-- Bootstrap core CSS -->
+<link href="./template_file/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="./template_file/sticky-footer-navbar.css" rel="stylesheet">
---
+++ views/main/include/js.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="./template_file/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="./template_file/popper.min.js"></script>
+<script src="./template_file/bootstrap.min.js"></script>
