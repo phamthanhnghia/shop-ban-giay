cbbb5b0263b5ce27b0334a64aae4a70d55291b3c
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-10 20:48:26 +0700

    _form

--- views/discount-product/_form.php
+++ views/discount-product/_form.php
@@ -18,13 +18,7 @@ $product = new Product();
 
     <?= $form->field($model, 'info')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'type')->dropDownList($discount->getArrayType()) ?>
-
     <?= $form->field($model, 'discount')->textInput() ?>
-
-    <?= $form->field($model, 'status')->dropDownList($discount->getArrayStatus()) ?>
-
-    <?= $form->field($model, 'id_product')->dropDownList($product->getArrayProduct()) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
