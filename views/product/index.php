9cfc53fb583c5f50bccbfe4c4ee989a6465db5bb
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-12 01:17:17 +0700

    index

--- views/product/index.php
+++ views/product/index.php
@@ -32,7 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
                          return $model->price . " VNĐ";
                         },
             ],
-            //'price',
+            //'amount',
             //'gender',
             
             //'created_date',
