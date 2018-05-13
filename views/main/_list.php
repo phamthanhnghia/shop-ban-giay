0a87acc034bfa56f7a1a1d2eaf1a005138218a81
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-13 16:19:26 +0700

    list

--- views/main/_list.php
+++ views/main/_list.php
@@ -37,7 +37,7 @@ use yii\widgets\LinkPager;
                     <small><?= $key->code ?></small>
 	                <h3><a href="#"><?= $key->name ?> </a></h3>
 	                <p>Giá : <strong><?= number_format($key->price) ?> VNĐ</strong>  </p>
-	                <p><button class="btn btn-success" >Thêm vào giỏ </button> <a href="../../main/detail?id=<?php echo $key->id; ?>" class="btn btn-primary" role="button">Xem chi tiết</a></p>
+	                <p><button onclick="addToBasket(<?= $key->id ?>)" class="btn btn-success" >Thêm vào giỏ </button> <a href="<?php echo Yii::$app->request->baseUrl.'/main/detail?id='. $key->id ; ?>" class="btn btn-primary" role="button">Xem chi tiết</a></p>
 	            </div>
 	        </div>
 	    </div>
@@ -49,20 +49,55 @@ echo LinkPager::widget([
     'pagination' => $pages,
 ]);
 ?>
+<style type="text/css">
+    [data-notify="progressbar"] {
+        margin-bottom: 0px;
+        position: absolute;
+        bottom: 0px;
+        left: 0px;
+        width: 100%;
+        height: 5px;
+    }
+
+</style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="<?php echo Yii::$app->homeUrl.'bootstrap-growl/jquery.bootstrap-growl.js'?>"></script>
 <script type="text/javascript">
-    $("button").click(function(){
+    //$("button").click(function(){
+        // $.ajax({
+        //        url: '<?php echo Yii::$app->request->baseUrl. '/product/test' ?>',
+        //        type: 'post',
+        //        data: {
+                         
+        //                  _csrf : '<?=Yii::$app->request->getCsrfToken()?>',
+        //                  id : "sđfsd"
+        //              },
+        //        success: function (data) {
+        //           // console.log(data.code);
+        //            console.log(data);
+        //           // console.log("ok");
+        //            //alert(data);
+        //        }
+        //   });
+            
+
+    //    });
+    function addToBasket(id) {
+        // console.log(id);
         $.ajax({
-               url: '<?php echo Yii::$app->request->baseUrl. '/product/test' ?>',
+               url: '<?php echo Yii::$app->request->baseUrl. '/product/add-basket' ?>',
                type: 'post',
                data: {
-                         
                          _csrf : '<?=Yii::$app->request->getCsrfToken()?>',
-                         nghia : "sđfsd"
+                         id : id
                      },
                success: function (data) {
-                  console.log(data.code);
+                   //console.log(data);
+                   alert("Thêm sản phẩm vào giỏ hàng thành công !");
+ 
                }
-          });
-        });
+                });
+        }
+    
+    
 </script>
