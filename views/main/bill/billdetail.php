1be83224c4ab5e05e668e1be8aaaeb70c1368503
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-23 16:15:17 +0700

    bill detail

---
+++ views/main/bill/billdetail.php
@@ -0,0 +1,125 @@
+<?php
+use app\models\User;
+use app\models\Product;
+use app\models\Bill;
+use app\models\BillDetail;
+
+
+$product = new Product();
+$id_bill = $_GET['id'];
+$bill = Bill::findOne(['id' => $id_bill]);
+$list_detail = BillDetail::find()->where(['id_bill' => $id_bill])->all();
+?>
+
+<div class="container">
+    <div class="row">
+    	<h4>Chi tiết hoá đơn : <b><?php echo $bill->bill_code; ?></b></h4>
+    	<hr style="height:1px;border:none;color:#333;background-color:#333;">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Sản phẩm</th>
+                        <th>Số lượng</th>
+                        <th class="text-center">Giá</th>
+                        <th> </th>
+                        <th class="text-center">Tiền</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                	<?php 
+                	foreach ($list_detail as $key => $value) {
+                		$product = Product::findOne(['id' => $value->id_product]);
+                		?>
+                		<tr>
+                        <td class="col-sm-8 col-md-6">
+                        <div class="media">
+                            <a class="thumbnail pull-left" href="#"> <img class="media-object" src="../../images/product-images/<?php echo $product->showImage($value->id_product) ?> " style="width: 72px; height: 72px;"> </a>
+                            <div class="media-body">
+	                            <h4 class="media-heading"><a href="#"><?php echo $product->name; ?></a></h4>
+	                            <h5 class="media-heading"> Size <a href="#"><?php echo $value->size_product; ?></a></h5>
+	                            <span>Khuyến mãi: </span>
+	                            <span class="text-success">
+	                                <strong>
+	                                    <?php echo $product->getDiscountProductsDiscount() . "%"; ?>
+	                                </strong>
+	                                <?php echo $product->getDiscountProductsInfo(); ?>
+	                            </span>
+	                        </div>
+                        </div></td>
+                        <td class="col-sm-1 col-md-1" style="text-align: center">
+                        <strong><?php echo $value->amount; ?></strong>
+                        </td>
+                        <td class="col-sm-1 col-md-1 text-center"><strong><?= number_format($product->price) ?> VNĐ</strong></td>
+                        <td class="col-sm-1 col-md-1">
+                        </td>
+                        <td class="col-sm-1 col-md-1 text-center"><strong><?= number_format($value->sum_price) ?> VNĐ</strong></td>
+                        
+                    </tr>
+
+                		<?php
+
+                	}
+                	?>
+                    
+                   
+                   
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                    </tr>
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td><h3>Tổng tiền</h3></td>
+                        <td class="text-right"><h3><strong><?= number_format($bill->total_price) ?> VNĐ</strong></h3></td>
+                    </tr>
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>
+                        	<?php
+                        		if($bill->status == 1){
+                        			?>
+                        			<a href="<?php  echo Yii::$app->urlManager->createUrl("main/remove-bill/?id=" .$id_bill); ?>">
+                        				<button type="button" class="btn btn-danger">
+                            	 				Huỷ đơn hàng
+                        				</button>
+                        			</a>
+                        				
+                        			<?php
+                        		}
+
+                        	?>
+                        	<?php
+                        		if($bill->status == 2){
+                        			?>
+                        			<a href="<?php  echo Yii::$app->urlManager->createUrl("main/hide-bill/?id=" .$id_bill); ?>">
+                        				<button type="button" class="btn btn-danger">
+                            	 				Xoá lịch sử đơn hàng
+                        				</button>
+                        			</a>
+                        				
+                        			<?php
+                        		}
+
+                        	?>
+                        </td>
+                        <td>
+                        	<a href="<?php  echo Yii::$app->urlManager->createUrl("main/bill"); ?>" <button type="button" class="btn btn-success">
+                            Quay lại <span class="glyphicon glyphicon-play"></span>
+                        </button></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+</div>
