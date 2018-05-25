4c07dff8fc955972f69e46fc1e07cc64a820c7f2
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-25 12:55:03 +0700

    index

--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,71 @@
-<h1>home</h1>
+<?php 
+use app\models\User;
+use app\models\Product;
+use app\models\Bill;
+
+$product = new Product();
+$bill = new  Bill();
+$user = new User();
+?>
+<div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3><?= $bill->countBillNewOrder() ?></h3>
+
+              <p>Hoá đơn mới</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">Xem thêm <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3><?= $bill->rateBill() ?><sup style="font-size: 20px">%</sup></h3>
+
+              <p>Tỉ lệ hoàn thành</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">Xem thêm <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3><?= $user->countUser() ?></h3>
+
+              <p>Khách hàng đăng kí</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">Xem thêm <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Số lượng khách truy cậps</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">Xem thêm <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
